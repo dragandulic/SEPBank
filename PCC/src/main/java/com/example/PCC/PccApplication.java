b393c97893c7f43481da7b683b09c14cd6652190
aleksandragr <grujica995@gmail.com>
2019-01-26 18:35:46 +0100

    pcc

---
+++ PCC/src/main/java/com/example/PCC/PccApplication.java
@@ -0,0 +1,14 @@
+package com.example.PCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PccApplication.class, args);
+	}
+
+}
+
