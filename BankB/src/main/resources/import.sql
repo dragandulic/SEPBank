9ec78470f6261c2664415243dc26d3e98eaf404a
dragandulic <35114903+dragandulic@users.noreply.github.com>
2019-01-30 20:42:15 +0100

    fix

--- BankB/src/main/resources/import.sql
+++ BankB/src/main/resources/import.sql
@@ -1,3 +1,5 @@
 INSERT INTO `bankb`.`merchant` (`id`, `name`, `merchant_id`, `merchant_password`, `bankaccount_id`) VALUES ('1','Filomat', '8h^ygfRESG6tfgdd456/NHFD','password@4',1);
+INSERT INTO `bankb`.`merchant` (`id`, `name`,`merchant_id`,`merchant_password`,`bankaccount_id`) VALUES ('2','Saskin magazin','411sldmcv81yt6j5nf9q2nv0m4u7v9','2',2);
 
-INSERT INTO `bankb`.`bank_account` (`id`, `numberofaccount`,`sum`) VALUES ('1','222-4444444444444-44','40000');
+INSERT INTO `bankb`.`bank_account` (`id`, `numberofaccount`,`sum`) VALUES ('1','411-4444444444444-44','40000');
+INSERT INTO `bankb`.`bank_account` (`id`, `numberofaccount`,`sum`) VALUES ('2','411-4444444444444-44','50000');
